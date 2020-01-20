bda2f798dc1e085c0024922f97d1f7eee84f244b
hautbois1011 <subterranean_mirror@yahoo.co.jp>
2020-01-20 14:00:57 +0900

    002 step

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ pub fn reverse_str(s: &str) -> String {
     s.chars().rev().collect()
 }
 
+pub fn step_str(s: &str, step: usize) -> String {
+    s.chars().step_by(step).collect()
+}
+
 #[cfg(test)]
 mod chap1_tests {
     use super::*;
@@ -10,6 +14,11 @@ mod chap1_tests {
     #[test]
     fn reverse_str_test() {
         assert_eq!("desserts".to_string(), reverse_str("stressed"));
+    }
+
+    #[test]
+    fn step_str_test() {
+        assert_eq!("パトカー".to_string(), step_str("パタトクカシーー", 2))
     }
 
 }
