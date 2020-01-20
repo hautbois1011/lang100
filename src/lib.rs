baadedddf6aaae8f76e3754b7da493faa64778a7
hautbois1011 <subterranean_mirror@yahoo.co.jp>
2020-01-20 14:15:31 +0900

    003 alternate

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,20 @@ pub fn reverse_str(s: &str) -> String {
     s.chars().rev().collect()
 }
 
+/// step毎に文字を抜き出します。
 pub fn step_str(s: &str, step: usize) -> String {
     s.chars().step_by(step).collect()
+}
+
+/// s1とs2の文字を交互に連結します。
+pub fn alternate_str(s1: &str, s2: &str) -> String {
+    let mut ret = String::new();
+    s1.chars().zip(s2.chars()).for_each(|(c1, c2)| {
+        ret.push(c1);
+        ret.push(c2);
+    });
+
+    ret
 }
 
 #[cfg(test)]
@@ -19,6 +31,11 @@ mod chap1_tests {
     #[test]
     fn step_str_test() {
         assert_eq!("パトカー".to_string(), step_str("パタトクカシーー", 2))
+    }
+
+    #[test]
+    fn alternate_str_test() {
+        assert_eq!("パタトクカシーー".to_string(), alternate_str("パトカー", "タクシー"))
     }
 
 }
